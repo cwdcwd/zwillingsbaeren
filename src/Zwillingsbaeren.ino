e6616db2fa79dfccbb6040de82f6a8f26718f431
CWD <cwd@lazybaer.com>
2018-07-29 23:17:24 -0400

    working with new relays

--- src/Zwillingsbaeren.ino
+++ src/Zwillingsbaeren.ino
@@ -1,58 +1,98 @@
 /*
  * Project Zwillingsbaeren
- * Description:
- * Author:
- * Date:
+ * Description: Dual Garage Door Opener
+ * Author: CWD <cwd@lazybaer.com>
+ * Date: 20180725
  */
+  const int EXECUTE_DELAY=750;
 
- int openDoor(String command);
- int closeDoor(String command);
- int controlPin=D0;
- int relayPower=A0;
+  int openDoor(String doorId);
+  int closeDoor(String doorId);
 
- int iGarageState=0;
- int iLastState;
+  int controlPins[]={ D0, D1 };
+  int doorStates[2];
+  int doorLastStates[2];
+
+  int statusLED = D7;
+
 
  void setup() {
      Serial.begin(9600);
-     iGarageState=iLastState=0;
+     //iGarageState01=iLastState01=0;
      Spark.function("openDoor",openDoor);
      Spark.function("closeDoor",closeDoor);
-     Spark.variable("state", &iGarageState, INT);
+     Spark.variable("state01", &doorStates[0], INT);
+     Spark.variable("state02", &doorStates[1], INT);
 
-     pinMode(controlPin, OUTPUT);
-     pinMode(relayPower, OUTPUT);
+     pinMode(controlPins[0], OUTPUT);
+     pinMode(controlPins[1], OUTPUT);
+     pinMode(statusLED, OUTPUT);
 
-     digitalWrite(controlPin, LOW);
-     digitalWrite(relayPower, HIGH);
+     digitalWrite(controlPins[0], HIGH);
+     digitalWrite(controlPins[1], HIGH);
+     digitalWrite(statusLED, LOW);
  }
 
  void loop() {
-     String s=((iGarageState==0)?"closed":"open");
+/*     String s=((iGarageState01==0)?"closed":"open");
 
-     if(iLastState!=iGarageState){
+     if(iLastState01!=iGarageState01){
          Spark.publish("state",s);
          Serial.println(s);
      }
 
-     iLastState=iGarageState;
-     delay(1000);
+     iLastState01=iGarageState01;
+     delay(1000); */
  }
 
- int openDoor(String command){
+ int openDoor(String doorId){
      //CWD-- execute open
-     iGarageState=1;
-     digitalWrite(controlPin,HIGH);
-     delay(750);
-     digitalWrite(controlPin,LOW);
-     return iGarageState;
+     Spark.publish("operation", "open:"+doorId);
+     int cp=0;
+
+     // check door state first to see if it's open. bail if so
+
+     if(doorId=="1") {
+       cp=controlPins[0];
+     } else if(doorId=="2") {
+       cp=controlPins[1];
+     } else {
+       return -1;
+     }
+
+     return executeDoor(cp);
  }
 
- int closeDoor(String command){
-     //CWD-- execute close
-     iGarageState=-1;
-     digitalWrite(controlPin,HIGH);
-     delay(750);
-     digitalWrite(controlPin,LOW);
-     return iGarageState;
+ int closeDoor(String doorId){
+   //CWD-- execute close
+   Spark.publish("operation", "close:"+doorId);
+   int cp=0;
+
+   // check door state first to see if it's closed. bail if so
+String state = ""; //CWD-- TODO: record state
+   if(state == "") {
+     if(doorId == "1") {
+       cp=controlPins[0];
+     } else if(doorId=="2") {
+       cp=controlPins[1];
+     } else {
+       Spark.publish("operation", "failed to open:"+doorId);
+       return -1;
+     }
+
+     return executeDoor(cp);
+  }
+ }
+
+ int executeDoor(int controlPin) {
+   Serial.println(controlPins[0]);
+   Serial.println(controlPins[1]);
+   Serial.print("executing Door on ");
+   Serial.println(controlPin);
+   digitalWrite(controlPin,LOW);
+   digitalWrite(statusLED, HIGH);
+   delay(EXECUTE_DELAY);
+   digitalWrite(controlPin,HIGH);
+   digitalWrite(statusLED, LOW);
+   return 1;
  }
